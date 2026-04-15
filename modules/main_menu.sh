b0b73611672e68f4ce1c9d63de04740ee64edbd7
Eng-Ayman-Mohamed <eng.ayman.mohamed.abotaha.kasim@gmail.com>
2026-04-15 13:26:23 +0200

    initial menus setup

---
+++ modules/main_menu.sh
@@ -0,0 +1,34 @@
+
+
+while true;
+do
+clear
+    echo "=============================== Current Database: ${CURRENT_DB:-None} ==============================="
+    echo "======================================"
+    echo "============ Main Menu ==============="
+    echo "======================================"
+    select choice in "Manage Students" "Manage Subjects" "Manage Grades" "Reports & Statistics" "Exit"
+    do
+    case $choice in
+    "Manage Students")
+    source ./modules/students/students_menu.sh
+    break
+    ;;
+    "Manage Subjects")
+    source ./modules/subjects/subjects_menu.sh
+    break
+    ;;
+    "Manage Grades")
+    source ./modules/grades/grades_menu.sh
+    break
+    ;;
+    "Reports & Statistics")
+    source ./modules/reports/reports_menu.sh
+    break
+    ;;
+    "Exit")
+    Exit 0
+    ;;
+    esac
+    done
+done
--- sgms.sh
+++ sgms.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+
+#initial setup 
+DB_DIR="./sgms_data"
+if [ ! -D "$BD_DIR"];
+then 
+    mkdir "$BD_DIR"
+fi
+
+source ./modules/main_menu.sh
