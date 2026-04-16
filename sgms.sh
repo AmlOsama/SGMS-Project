c32caa5b940a5da8fa2bbfa640aa7137af153a74
Amal Osama <amlosama014@gmail.com>
2026-04-16 18:52:38 +0200

    Refactor initial setup in sgms.sh to ensure directory creation and improve readability

--- sgms.sh
+++ sgms.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-#initial setup 
+# Initial setup
 DB_DIR="./sgms_data"
-if [ ! -D "$BD_DIR"];
-then 
-    mkdir "$BD_DIR"
-fi
 
+# -p creates parent directories and doesn't error if they exist
+mkdir -p "$DB_DIR/students"
+mkdir -p "$DB_DIR/subjects"
+mkdir -p "$DB_DIR/grades"
+
+# load modules
 source ./modules/main_menu.sh
+
+
+main_menu
