21d852112b053f9cef680cfc7fdd28864d6a355e
Eng-Ayman-Mohamed <eng.ayman.mohamed.abotaha.kasim@gmail.com>
2026-04-16 21:31:29 +0200

    delete student logic

--- modules/students/delete_student.sh
+++ modules/students/delete_student.sh
@@ -1,1 +1,20 @@
 #!/bin/bash
+
+read -p "Enter the student ID: " stu_id
+#current IDs
+if [[ -f "$TABLE_DATA_PATH/$stu_id.stu" ]];
+then
+    printStudent $TABLE_DATA_PATH/$stu_id.stu
+    read -p "Are you sure ? y/n" flag
+    if [[ $flag == "y" || $flag == "Y" ]]
+    then
+        rm -f $TABLE_DATA_PATH/$stu_id.stu
+        echo "Student was deleted"
+        read -rp "Press Enter to continue..."
+    fi
+else
+    echo "Student with this id doesn't exist"
+    read -rp "Press Enter to continue..."
+
+fi
+
